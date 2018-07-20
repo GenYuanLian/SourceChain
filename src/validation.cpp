0570e114b1418758bf1e4a07ec8ad8c920157597
xd <xiaodong@earthledger.com>
2018-07-20 18:08:48 +0800

    fix halving err

--- src/validation.cpp
+++ src/validation.cpp
@@ -1125,11 +1125,9 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     {
     	nSubsidy = NEW_BASE_SUBSIDY * COIN;
     }
-    else
-    {
-    	// Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    	nSubsidy >>= halvings;
-    }
+
+    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    nSubsidy >>= halvings;
 
     return nSubsidy;
 }
